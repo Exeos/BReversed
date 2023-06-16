8deefb8092e8f92e7d0cc3da945d738551bf7801
Exeos <69673405+Exeos@users.noreply.github.com>
2023-06-16 16:21:43 +0200

    added bozar string detector

--- src/main/java/xyz/breversed/detectors/bozar/BozarString.java
+++ src/main/java/xyz/breversed/detectors/bozar/BozarString.java
@@ -1,6 +1,13 @@
 package xyz.breversed.detectors.bozar;
 
-import xyz.breversed.api.asm.detection.Detector;
+import xyz.breversed.api.asm.detection.AbstractDetector;
+import xyz.breversed.api.asm.pattern.PatternParts;
 
-public class BozarString extends Detector {
+public class BozarString extends AbstractDetector implements PatternParts {
+
+    @Override
+    protected boolean detect() {
+        /* Always come together */
+        return new BozarNumber().detect();
+    }
 }
