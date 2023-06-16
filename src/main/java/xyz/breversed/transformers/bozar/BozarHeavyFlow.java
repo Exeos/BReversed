cdf457c2e681b792fa8d614a8efbccfeabe4b0b7
Exeos <69673405+Exeos@users.noreply.github.com>
2023-06-16 16:58:39 +0200

    started bozar heavy flow, will finish later

--- src/main/java/xyz/breversed/transformers/bozar/BozarHeavyFlow.java
+++ src/main/java/xyz/breversed/transformers/bozar/BozarHeavyFlow.java
@@ -1,14 +1,13 @@
 package xyz.breversed.transformers.bozar;
 
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
 import xyz.breversed.api.asm.pattern.PatternParts;
 import xyz.breversed.api.asm.pattern.PatternScanner;
 import xyz.breversed.api.asm.pattern.result.InsnResult;
 import xyz.breversed.api.asm.transformer.Transformer;
 import xyz.breversed.api.asm.utils.ASMUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BozarHeavyFlow extends Transformer implements PatternParts {
@@ -19,32 +18,61 @@ public class BozarHeavyFlow extends Transformer implements PatternParts {
 
         for (ClassNode classNode : getClasses()) {
             /* Bozar always adds this field */
-            FieldNode flowField = ASMUtil.getField(classNode, String.valueOf((char)5096), "J");
+            FieldNode flowField = ASMUtil.getField(classNode, String.valueOf((char)5097), "J");
             if (flowField == null)
                 continue;
 
             classNode.fields.remove(flowField);
+            for (MethodNode methodNode : classNode.methods) {
+                List<AbstractInsnNode> toRemove = new ArrayList<>();
+                for (InsnResult result : before(patternScanner, methodNode)) {
+                    AbstractInsnNode current = result.getLast();
+                    LabelNode dltLabel = ((LookupSwitchInsnNode) current).dflt;
 
+                    while ((current = current.getNext()) != dltLabel) {
+                        toRemove.add(current);
+                    }
+                    toRemove.add(current);
+                    while ((current = current.getNext()).getType() != AbstractInsnNode.LABEL) {
+                        toRemove.add(current);
+                    }
+                    current = current.getNext();
+                    int amount = switch (current.getOpcode()) {
+                        case LXOR -> 10;
+                        case LCMP -> ASMUtil.getNext(current, 6).getOpcode() == IF_ICMPNE ? 6 : 3;
+                        case LAND -> 3;
+                        default -> 0;
+                    };
+                    if (amount == 0) {
+                        System.out.println("FAILED!");
+                        continue;
+                    }
+
+                    for (int i = 0; i < amount + 1; i++) {
+                        toRemove.add(ASMUtil.getNext(current, i));
+                    }
+                }
+                System.out.println(toRemove.size());
+                ASMUtil.removeInsnNodes(methodNode, toRemove);
+            }
         }
     }
 
-    private List<InsnResult> before0switchB(PatternScanner patternScanner, MethodNode methodNode) {
+    private List<InsnResult> before(PatternScanner patternScanner, MethodNode methodNode) {
         patternScanner.setPattern(new int[] {
                 LABEL,
-                FIELD_INSN,
+                GETSTATIC,
                 LABEL,
-                P_ANY,
+                P_NUMBER,
                 GOTO,
                 LABEL,
-                P_ANY,
-                P_ANY,
+                P_NUMBER,
+                P_NUMBER,
                 LABEL,
-                //switch
                 DUP2,
                 L2I,
                 LOOKUPSWITCH
         });
         return patternScanner.scanMethod(methodNode);
     }
-
 }
